09b31dc6b6b19d7b309d84b5fe14c1918765a0c8
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-12-04 23:14:19 -0600

    Login and register is now working

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('tnTrails', []);
+var app = angular.module('tnTrails', ['Authorize', 'firebase', 'ngRoute']);
+
+app.config(
+	['$routeProvider',
+		function($routeProvider){
+			$routeProvider
+				.when('/', {
+					templateUrl: '/partials/login.html',
+					controller: 'authuserCtrl'
+				})
+				.otherwise({ redirectTo: '/login'});
+		}
+	]
+);
---
+++ javascripts/auth.js
@@ -0,0 +1,56 @@
+var auth = angular.module('Authorize', ['firebase']);
+
+	var name;
+	auth.factory("Auth", ["$firebaseAuth",
+	  function($firebaseAuth) {
+	    var ref = new Firebase("https://my-tn-trails.firebaseio.com/");
+
+	    return $firebaseAuth(ref);
+
+	  }
+	]);
+
+	//create a user
+	app.controller('authuserCtrl',
+		['$scope',
+		'Auth',
+		'$firebaseArray',
+		'$location',
+		function($scope, Auth, $currentAcct, $location){
+			$scope.createUser = function(){
+				Auth.$createUser({
+					email: $scope.email,
+					password: $scope.password
+				}).then(function(userData){
+					var ref = new Firebase("https://my-tn-trails.firebaseio.com/users/"+userData.uid+"/userinfo");
+					ref.set({
+						email: $scope.email,
+						name: $scope.name,
+						uid: userData.uid
+					});
+					console.log("user created with id:" +userData.uid);
+  					$scope.loginUser();
+				}).catch(function(error){
+					console.log("user not created with error:" +error);
+				});
+			};
+			$scope.loginUser = function(){
+				Auth.$authWithPassword({
+					email: $scope.email,
+					password: $scope.password
+				}).then(function(userData){
+					var ref = new Firebase("https://my-tn-trails.firebaseio.com/");
+					console.log("user logged in with id:" +userData.uid);
+					$location.path("/profile/");
+				}).catch(function(error){
+					console.log("user not logged with error:" +error);
+				});
+			};
+
+			$scope.logOut = function(){
+				Auth.$unauth();
+				console.log("user logged out");
+				//redirect back to login page
+				$location.path("/");
+			};
+		}]);
