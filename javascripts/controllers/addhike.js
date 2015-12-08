799cf0c994b699afcefcc22deb609a8befc04dd7
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-12-07 18:35:13 -0600

    flattening out my data structure

--- javascripts/controllers/addhike.js
+++ javascripts/controllers/addhike.js
@@ -5,7 +5,14 @@ app.controller('addhikeCtrl',
 	function(Auth, $addHikes, $location){
 		var authData = Auth.$getAuth();
 		console.log(authData);
+
+		// below is what i have currently and is working but should be flat data structure(hence branch flat_data_structure)
 		var ref = new Firebase("https://my-tn-trails.firebaseio.com/users/"+authData.auth.uid+"/hikes/");
+
+		// is the below how it should be?
+		// var ref = new Firebase("https://my-tn-trails.firebaseio.com/hikes/"+authData.auth.uid);
+
+
 
 		this.hikes = $addHikes(ref);
 
@@ -19,6 +26,7 @@ app.controller('addhikeCtrl',
 				rating: this.newHike.rating,
 				description: this.newHike.description,
 				id: "hike"+((Math.random()*10)+1)
+
 			});
 
 			// clarify that I need the below - creating new branch before going off on another tangent
